8bcebc6392d6cf84336185f7b67633d9dc5d2e9c
rudianto <rudianto619@gmail.com>
2017-01-30 16:22:48 +0700

    push add validation

--- application/controllers/Kandangcontroller.php
+++ application/controllers/Kandangcontroller.php
@@ -35,6 +35,9 @@ class Kandangcontroller extends CI_Controller {
 
 			$crud->set_theme('datatables');
 			$crud->set_table('t_kandang');
+			$crud->set_rules('no_kandang','no_kandang','required');
+			$crud->set_rules('jml_ayam','jml_ayam','required');
+			$crud->set_rules('id_vaksin','id_vaksin','required');
 			$crud->set_relation('id_vaksin','t_vaksin','nama_vaksin');
 			//$crud->display_as('officeCode','Office City');
 			//$crud->set_subject('Employee');
@@ -43,6 +46,7 @@ class Kandangcontroller extends CI_Controller {
 
 			//$crud->set_field_upload('file_url','assets/uploads/files');
 
+			
 			$output = $crud->render();
 
 		//$this->grocery_crud->set_table('t_kandang');
@@ -62,9 +66,10 @@ class Kandangcontroller extends CI_Controller {
 		$data['title']="Data Vaksin";
 		$this->grocery_crud->set_table('t_vaksin');
 		$this->grocery_crud->set_theme('datatables');
-		$this->grocery_crud->set_rules('nama_vaksin','varchar');
-		$this->grocery_crud->set_rules('jml_vaksin','harga_vaksin','integer');
-		$this->grocery_crud->set_rules('tanggal_vaksin','datetime');
+		$this->grocery_crud->set_rules('nama_vaksin','nama_vaksin','required');
+		$this->grocery_crud->set_rules('jml_vaksin','jml_vaksin','required');
+		$this->grocery_crud->set_rules('harga_vaksin','harga_vaksin','required');
+		$this->grocery_crud->set_rules('tanggal_vaksin','tanggal_vaksin','required');
 		$output = $this->grocery_crud->render();
 		
 		$this->load->view('header2',$output);
@@ -79,6 +84,9 @@ class Kandangcontroller extends CI_Controller {
 		$this->grocery_crud->set_theme('datatables');
 		$this->grocery_crud->set_relation('id_perusahaan','t_perusahaan','nama_perusahaan');
 		$this->grocery_crud->display_as('id_perusahaan','Nama perusahaan');
+		$this->grocery_crud->set_rules('nama_perusahaan','nama_perusahaan','required');
+		$this->grocery_crud->set_rules('nama_pelanggan','nama_pelanggan','required');
+		$this->grocery_crud->set_rules('alamat_pelanggan','alamat_pelanggan','required');
 		$output = $this->grocery_crud->render();
 		
 		$this->load->view('header2',$output);
@@ -91,11 +99,14 @@ class Kandangcontroller extends CI_Controller {
 		$data['title']="Data perusahaan";
 		$this->grocery_crud->set_table('t_perusahaan');
 		$this->grocery_crud->set_theme('datatables');
+		$this->grocery_crud->set_rules('nama_perusahaan','nama_perusahaan','required');
+		$this->grocery_crud->set_rules('alamat_perusahaan','alamat_perusahaan','required');
 		$output = $this->grocery_crud->render();
 		
 		$this->load->view('header2',$output);
 		$this->load->view('title',$data);
 		$this->load->view('kandang',$output);
+
 	}
 
 	public function pesan()
@@ -105,6 +116,12 @@ class Kandangcontroller extends CI_Controller {
 		$this->grocery_crud->set_theme('datatables');
 		$this->grocery_crud->set_relation('id_pelanggan','t_pelanggan','nama_pelanggan');
 		$this->grocery_crud->set_relation('id_perusahaan','t_perusahaan','nama_perusahaan');
+		$this->grocery_crud->set_rules('id_pelanggan','id_pelanggan','required');
+		$this->grocery_crud->set_rules('id_perusahaan','id_perusahaan','required');
+		$this->grocery_crud->set_rules('waktu_pemesanan','waktu_pemesanan','required');
+		$this->grocery_crud->set_rules('waktu_kirim','waktu_kirim','required');
+		$this->grocery_crud->set_rules('jumlah_pemesanan','jumlah_pemesanan','required');
+		$this->grocery_crud->set_rules('status_penerimaan','status_penerimaan','required');
 		$output = $this->grocery_crud->render();
 		
 		$this->load->view('header2',$output);
@@ -120,6 +137,9 @@ class Kandangcontroller extends CI_Controller {
 		$this->grocery_crud->columns('id_kandang','jumlah_telur','waktu');
 		$this->grocery_crud->display_as('id_kandang','No Kandang');
 		$this->grocery_crud->set_relation('id_kandang','t_kandang','no_kandang');
+		$this->grocery_crud->set_rules('id_kandang','id_kandang','required');
+		$this->grocery_crud->set_rules('jumlah_telur','jumlah_telur','required');
+		$this->grocery_crud->set_rules('waktu','waktu','required');
 		$output = $this->grocery_crud->render();
 
 		
